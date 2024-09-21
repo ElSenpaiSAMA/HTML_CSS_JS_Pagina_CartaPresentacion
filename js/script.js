88d2b066a0719dd7fc175fdb95e7272318a2b9ba
ElSenpai-SAMA <104552415+ElSenpaiSAMA@users.noreply.github.com>
2024-09-21 12:35:54 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-document.getElementById("cv").addEventListener("click", function() {
-    let enlaceDescarga = document.createElement("a");
-    enlaceDescarga.href = "archivos/CV.pdf"; 
-    enlaceDescarga.download = "CV.pdf"; 
-    enlaceDescarga.click();
-});
+
